9136070f4986dc5bd930b28418bf23b370b10afe
Muhammed Fatih Çolak <mfatihcolak1@gmail.com>
2022-02-20 02:19:12 +0300

    hatalar ve istisnalar ödevler

---
+++ Hatalar ve İstisnalar/Odev1.py
@@ -0,0 +1,8 @@
+listem = ["345324","fatih","colak","26","ısparta32","40.0"]
+
+for i in listem:
+    try:
+        i = int(i)
+        print(i)
+    except:
+        pass
---
+++ Hatalar ve İstisnalar/Odev2.py
@@ -0,0 +1,9 @@
+def ciftMi(a):
+
+ if (a % 2 != 0):
+    raise ValueError("Girdiğiniz sayı tek bir sayı")
+ else:
+    return a
+
+
+print(ciftMi(15))
